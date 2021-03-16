0fe15a9db8763c20bac87dc8fb7236b0dd747e20
Fiki Alamsyah <fiki@pop-os.localdomain>
2021-03-16 17:37:11 +0700

    resolved code

--- check_host.py
+++ check_host.py
@@ -1,16 +1,17 @@
-import subprocess
 import sys
+import subprocess
+
 
 
 if(len(sys.argv) <= 1):
     print("IP address belum diberikan")
 else:
-    cmd = ['ping', '-c1', sys.argv[1]]
-    res = subprocess.check_output(cmd)
-    if(res):
-        print('UP')
+    host_ip = str(sys.argv[1])
+    status, result = subprocess.getstatusoutput("ping -c1 " + host_ip)
+    if(status == 0):
+        print(f'Host {host_ip} is UP')
     else:
-        print('DOWN')
+        print(f'Host {host_ip} is DOWN')
 
 
 
