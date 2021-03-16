8c614fb495cc18a60e51f270ec24fae4c979066f
Fiki Alamsyah <fiki@pop-os.localdomain>
2021-03-16 15:36:29 +0700

    adding check_host.py

---
+++ check_host.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+
+
+if(len(sys.argv) <= 1):
+    print("IP address belum diberikan")
+else:
+    cmd = ['ping', '-c1', sys.argv[1]]
+    res = subprocess.check_output(cmd)
+    if(res):
+        print('UP')
+    else:
+        print('DOWN')
+
+
+
+
+
