e692036b6398bc81bc5fee9d44497f518ef8048a
fiki <fiki@pop-os.localdomain>
2021-03-30 15:26:03 +0700

    add paralel_host_threading.py

---
+++ paralel_host_thread.py
@@ -0,0 +1,29 @@
+import subprocess
+import concurrent.futures
+import time
+
+# waktu awal
+T1 = time.perf_counter()
+
+# list ip
+hosts = ['192.168.1.1', '192.168.1.2', '192.168.1.3', '8.8.8.8', '8.8.4.4']
+
+# fungsi check host
+def check_host(ip):
+    status, result = subprocess.getstatusoutput("ping -c1 " + ip)
+    if (status == 0):
+        return f'Host {ip} is UP'
+    else:
+        return f'Host {ip} is DOWN'
+
+# proses multithreading
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = executor.map(check_host, hosts)
+    for results in results:
+        print(results)
+
+# waktu akhir
+T2 = time.perf_counter()
+
+# cetak total waktu
+print(f"selesai dalam : {round(T2 - T1, 2)} detik")
